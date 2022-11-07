ad4afd00c111cb4a06a6c3ff0d5dd981145f87a3
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-11-08 02:27:39 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div>
-      {/* <footer class="p-4 bg-white sm:p-6 bg-gray-900 text-white">
+      <footer class="p-4 bg-white sm:p-6 bg-gray-900 text-white">
         <div class="grid grid-cols-2 lg:grid-cols-4 justify-center  text-white">
           <div class="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" class="flex items-center">
+            <Link to="https://flowbite.com/" class="flex items-center">
               <img
                 src="https://flowbite.com/docs/images/logo.svg"
                 class="mr-3 h-8"
@@ -15,7 +16,7 @@ const Footer = () => {
               <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
                 Flowbite
               </span>
-            </a>
+            </Link>
           </div>
           <div>
             <h2 class="mb-6 text-sm font-semibold  uppercase text-white">
@@ -23,14 +24,14 @@ const Footer = () => {
             </h2>
             <ul class=" text-gray-400">
               <li class="mb-4">
-                <a href="https://flowbite.com/" class="hover:underline">
+                <Link to="/" class="hover:underline">
                   Flowbite
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="https://tailwindcss.com/" class="hover:underline">
+                <Link to="/" class="hover:underline">
                   Tailwind CSS
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -40,17 +41,20 @@ const Footer = () => {
             </h2>
             <ul class=" text-gray-400">
               <li class="mb-4">
-                <a
-                  href="https://github.com/themesberg/flowbite"
+                <Link
+                  to="https://github.com/themesberg/flowbite"
                   class="hover:underline "
                 >
                   Github
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">
+                <Link
+                  to="https://discord.gg/4eeurUVvTy"
+                  class="hover:underline"
+                >
                   Discord
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -60,14 +64,14 @@ const Footer = () => {
             </h2>
             <ul class=" text-gray-400">
               <li class="mb-4">
-                <a href="#" class="hover:underline" alt="">
+                <Link to="/" class="hover:underline" alt="">
                   Privacy Policy
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" class="hover:underline">
+                <Link to="/" class="hover:underline">
                   Terms &amp; Conditions
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -77,14 +81,14 @@ const Footer = () => {
         <div class="sm:flex sm:items-center sm:justify-between lg:mx-10">
           <span class="text-sm  sm:text-center text-gray-200">
             © 2022{" "}
-            <a href="https://flowbite.com/" class="hover:underline">
+            <Link to="/" class="hover:underline">
               Flowbite™
-            </a>
+            </Link>
             . All Rights Reserved.
           </span>
           <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a
-              href="#"
+            <Link
+              to="/"
               class="text-gray-400 hover:text-gray-900 hover:text-white"
             >
               <svg
@@ -100,9 +104,9 @@ const Footer = () => {
                 ></path>
               </svg>
               <span class="sr-only">Facebook page</span>
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               class="text-gray-400 hover:text-gray-900 hover:text-white"
             >
               <svg
@@ -118,9 +122,9 @@ const Footer = () => {
                 ></path>
               </svg>
               <span class="sr-only">Instagram page</span>
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               class="text-gray-400 hover:text-gray-900 hover:text-white"
             >
               <svg
@@ -132,9 +136,9 @@ const Footer = () => {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
               <span class="sr-only">Twitter page</span>
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               class="text-gray-400 hover:text-gray-900 hover:text-white"
             >
               <svg
@@ -150,9 +154,9 @@ const Footer = () => {
                 ></path>
               </svg>
               <span class="sr-only">GitHub account</span>
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               class="text-gray-400 hover:text-gray-900 hover:text-white"
             >
               <svg
@@ -168,10 +172,10 @@ const Footer = () => {
                 ></path>
               </svg>
               <span class="sr-only">Dribbbel account</span>
-            </a>
+            </Link>
           </div>
         </div>
-      </footer> */}
+      </footer>
     </div>
   );
 };
